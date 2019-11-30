105c0e7c8775d6f59379a0ca62d9486432ddbdd2
R-Kawabe <dragontree7@gmail.com>
2019-11-30 17:22:30 +0900

    整理

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TopPage from './Page/TopPage';
+import MapPage from './Page/MapPage';
+import KeySearchPage from './Page/KeySearchPage';
+
+export default class App extends Comment {
+    constructor(props) {
+        super(props);
+
+    }
+}
