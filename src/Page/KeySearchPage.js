3c210c4762f08e12e82eb39f65b5ceb539fae437
R-Kawabe <dragontree7@gmail.com>
2019-12-06 23:06:17 +0900

    test

--- src/Page/KeySearchPage.js
+++ src/Page/KeySearchPage.js
@@ -14,43 +14,46 @@ export default class KeySearchPage extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {//addEventListener的な、このファイルが読み込まれたときに実行
+        //
+    }
+
     handleChange(event) { //stateに入力値をセットする
         this.setState({ value: event.target.value });
     }
+
     handleSubmit(event) {
         alert('hoge:' + this.state.value);
         event.preventDefault();
     }
 
-    hoge() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
-
-    huga() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <select type="select" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
+    //ドロップダウンリストは、課題、目的、対象のそれぞれについてを作成したい
+    //ドロップダウンリストのoptionは、クエリを発行して返ってきたトリプルを整形して組み込みたい
+    //
 
     render() {
         return (
-            <div>{this.hoge()}
-                <br />
-                {this.huga()}</div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="text">
+                        <label>
+                            Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </div>
+
+                    <div className="select">
+                        <label>
+                            kadai:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                                {Object.keys(/*返ってきたデータ*/).map(k => <option key={k}>{/*データ*/[k]}</option>)}
+                            </select>
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
